d74160746e79d8ddeb28146acba2f2c9ef788b33
Dmitri Kunin <info@dkunin.ru>
2015-08-12 21:01:15 +0300

    Move config file to localfolder

--- index.js
+++ index.js
@@ -2,10 +2,32 @@
 
 'use strict';
 
-var arnotif      = require('autoremotenotification');
-var argv         = require('yargs').argv;
-var defaultNotif = {title: 'Tilte', text:'Text'};
-var R            = require('ramda');
+var arnotif              = require('autoremotenotification');
+var argv                 = require('yargs').argv;
+var defaultNotif         = {title: 'Tilte', text:'Text'};
+var R                    = require('ramda');
+var nconf                = require('nconf');
+var path                 = require('path');
+var autoremoteConfigFile = path.join(__dirname, 'autoremotekey.json');
+ 
+nconf.argv()
+     .env()
+     .file({ file: autoremoteConfigFile });
+
+var apiKey = nconf.get('apikey');
+var config = nconf.get('config');
+
+if(config) {
+  var data = config.split('=');
+  nconf.set(data[0], data[1]);
+  nconf.save(function (err) {
+    if(err) {
+      throw new Error(err);
+    }
+  });
+  console.log('settings updated');
+  return;
+}
 
 if(argv.m) {
   argv.text = argv.m;
@@ -13,8 +35,8 @@ if(argv.m) {
 if(argv.t) {
   argv.title = argv.t;
 }
-if(argv.k) {
-  argv.key = argv.k;
-}
+
+argv.key = apiKey;
+
 
 arnotif(R.merge(defaultNotif, argv));
